11670856124b51e99855ba7bce856aac0785e4de
Unknown <regoczi.tamas01@gmail.com>
2018-07-25 13:40:12 +0200

    user registration feature implemented

--- src/main/java/com/codecool/shop/process/CheckoutProcess.java
+++ src/main/java/com/codecool/shop/process/CheckoutProcess.java
@@ -6,11 +6,12 @@ import org.json.JSONObject;
 public class CheckoutProcess {
 
     public static boolean checkout(Order order, String name, String phoneNumber, String email, JSONObject saddress, JSONObject baddress) {
-        User user = User.getInstance();
+       /* User user = User.getInstance();
         user.setUserDataForCheckout(name, phoneNumber, email, Address.parseToAddress(saddress), Address.parseToAddress(baddress));
         order.setUser(user);
         order.setStatus(Status.CHECKED_OUT);
-        return true;
+        return true;*/
+       return true;
     }
 
 }
