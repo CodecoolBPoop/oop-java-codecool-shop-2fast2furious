08d17c8b74c647c4732b1c8a9402c329c4a8b7d7
Unknown <regoczi.tamas01@gmail.com>
2018-07-26 15:24:52 +0200

    checkout process updated

--- src/main/java/com/codecool/shop/process/CheckoutProcess.java
+++ src/main/java/com/codecool/shop/process/CheckoutProcess.java
@@ -1,16 +1,17 @@
 package com.codecool.shop.process;
 
+import com.codecool.shop.dao.implementation.OrderDaoSQL;
 import com.codecool.shop.model.*;
 import org.json.JSONObject;
 
 public class CheckoutProcess {
 
     public static boolean checkout(Order order, String name, String phoneNumber, String email, JSONObject saddress, JSONObject baddress) {
-       /* User user = User.getInstance();
-        user.setUserDataForCheckout(name, phoneNumber, email, Address.parseToAddress(saddress), Address.parseToAddress(baddress));
-        order.setUser(user);
-        order.setStatus(Status.CHECKED_OUT);
-        return true;*/
+
+       order.setStatus(Status.CHECKED_OUT);
+       order.setShippingAddress(Address.parseToAddress(saddress));
+       order.setBillingAddress(Address.parseToAddress(baddress));
+       OrderDaoSQL.getInstance().uploadOrderWithCart(order);
        return true;
     }
 
