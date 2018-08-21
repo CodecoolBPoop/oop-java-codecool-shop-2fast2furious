0acda739f73423450171c42b86021d72d2742526
Unknown <regoczi.tamas01@gmail.com>
2018-08-21 13:40:38 +0200

    orderhistory class created

---
+++ src/main/java/com/codecool/shop/controller/OrderHistory.java
@@ -0,0 +1,30 @@
+package com.codecool.shop.controller;
+
+import com.codecool.shop.config.TemplateEngineUtil;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = {"/orderhistory"})
+public class OrderHistory extends HttpServlet {
+
+
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) {
+        TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
+        WebContext context = new WebContext(req, resp, req.getServletContext());
+
+
+
+
+
+    }
+
+
+
+}
