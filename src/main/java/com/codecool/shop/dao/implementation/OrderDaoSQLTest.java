f40512ec780ece9f352b017926616e16fe62637d
Unknown <regoczi.tamas01@gmail.com>
2018-08-23 13:14:02 +0200

    fixed imports

--- src/main/java/com/codecool/shop/dao/implementation/OrderDaoSQLTest.java
+++ src/main/java/com/codecool/shop/dao/implementation/OrderDaoSQLTest.java
@@ -1,8 +1,10 @@
 package com.codecool.shop.dao.implementation;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+
+import static org.junit.Assert.assertTrue;
+
 
 class OrderDaoSQLTest {
     @Test
